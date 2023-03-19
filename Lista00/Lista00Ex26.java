8aff23804d4af7637c8e8f899194d49386eb8ac6
Jéssica Aquiles <73039194+JEZIGA01@users.noreply.github.com>
2023-03-18 22:36:41 -0300

    fixed: Ordenando a matriz decrescente

--- Lista00/Lista00Ex26.java
+++ Lista00/Lista00Ex26.java
@@ -7,64 +7,62 @@
 import java.util.Scanner;
 public class Lista00Ex26 {
 	
-	public static void main (String[] args) {
-		Scanner teclado = new Scanner(System.in);
-        
-        System.out.print("Digite o numero de linhas: ");
-        int N = teclado.nextInt();
-        System.out.print("Digite o numero de colunas: ");
-        int M = teclado.nextInt();
-        
-        int O[][] = new int [N][M];
-		int ODecrescente[][] = new int [N][M];
-		int aux = 0;
-		
-		
-         for (int linhas = 0 ; linhas < O.length ; linhas++) {
-            for (int colunas = 0 ; colunas < O[linhas].length ; colunas++) {
-                
-			int Random = (int) (Math.random() * 100 );    
-			O[linhas][colunas] = Random;
-			ODecrescente[linhas][colunas] = O[linhas][colunas];
-				
-            for (int linhas2 = 0 ; linhas2 < O.length ; linhas2++) {
-				for (int colunas2 = 0 ; colunas2 < O[linhas2].length ; colunas2++) {
-				
-              
-					if(ODecrescente[linhas][colunas] >  ODecrescente[linhas2][colunas2]){
-					
-					aux = ODecrescente[linhas][colunas];
-					ODecrescente[linhas][colunas] = ODecrescente[linhas2][colunas2] ;
-					ODecrescente[linhas2][colunas2] = aux;
-					
-					}
-				}
-			}
-		}
-	}
-       
-        System.out.println("/////////////////// Matriz nao ordenada ///////////////////");
-        
-        for (int i = 0 ; i < O.length ; i++) {
-            for (int j = 0 ; j < O[i].length ; j++) {
-                
-               System.out.print(O[i][j] + "\t"); 
-                
-               
-                }
-                System.out.println();
-            }
-            
-            System.out.println("\n\n/////////////////// Matriz em ordem Decrescente ///////////////////");
-             for (int i = 0 ; i < ODecrescente.length ; i++) {
-				for (int j = 0 ; j < ODecrescente[i].length ; j++) {
-                
-               System.out.print(ODecrescente[i][j] + "\t"); 
-                
-               
-                }
-                System.out.println();
-            }  
-	}
-}
+	 public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
 
+        System.out.print("Digite o numero de linhas: ");
+        int nLinha = teclado.nextInt();
+        System.out.print("Digite o numero de colunas: ");
+        int nColuna = teclado.nextInt();
+
+        int matriz[][] = new int[nLinha][nColuna];
+        int ODecrescente[][] = new int[nLinha][nColuna];
+        int aux = 0;
+
+        // aqui iremos fazer a matriz com os numeros aleatorios linhas e colunas
+        for (int linhas = 0; linhas < matriz.length; linhas++) {
+            for (int colunas = 0; colunas < matriz[linhas].length; colunas++) {
+                int Random = (int) (Math.random() * 100);
+                matriz[linhas][colunas] = Random;
+                ODecrescente[linhas][colunas] = matriz[linhas][colunas];
+            }
+        }
+
+
+        for (int linhas = 0 ; linhas < ODecrescente.length ; linhas++) {
+            for (int colunas = 0 ; colunas < ODecrescente[linhas].length ; colunas++) {
+                int Random = (int) (Math.random() * 100 );
+                matriz[linhas][colunas] = Random;
+                ODecrescente[linhas][colunas] = matriz[linhas][colunas];
+                for (int linhas2 = 0 ; linhas2 < matriz.length ; linhas2++) {
+                    for (int colunas2=0; colunas2 < ODecrescente[linhas2].length ; colunas2++) {
+                        if(ODecrescente[linhas][colunas] > ODecrescente[linhas2][colunas2]){
+                            aux = ODecrescente[linhas][colunas];
+                            ODecrescente[linhas][colunas] = ODecrescente[linhas2][colunas2] ;
+                            ODecrescente[linhas2][colunas2] = aux;
+
+                        }
+                    }
+                }
+            }
+        }
+
+        // Imprimindo a matriz não ordenada
+        System.out.println("/////////////////// Matriz nao ordenada ///////////////////");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        // Imprimindo a matriz ordenada em ordem decrescente
+        System.out.println("\n\n/////////////////// Matriz em ordem Decrescente ///////////////////");
+        for (int i = 0; i < ODecrescente.length; i++) {
+            for (int j = 0; j < ODecrescente[i].length; j++) {
+                System.out.print(ODecrescente[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
